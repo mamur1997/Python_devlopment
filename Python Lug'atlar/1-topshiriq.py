1c99606c5e6f9aba911a9c331014c32895109362
mamur.zakirov <108054644+mamur1997@users.noreply.github.com>
2025-04-18 16:09:38 +0500

    new depository

---
+++ Python Lug'atlar/1-topshiriq.py
@@ -0,0 +1,26 @@
+    #(Otam onam,men va) degan lug'at yarating
+#va lug'atga shu inson haqida kamida 3 ta m'alumot kiriting
+#(ismi, tu'gilgan yili, shahri, manzili va hokazo). 
+#Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring
+#:Otamning ismi Mavlutdin, 1954-yilda, Samarqand viloyatida tug'ilgan
+
+Otam={
+    'Ism':'Tolibjon',
+    'Tugilgan_yili':'1971-yil',
+    'Manzil':'Namangan',   
+    }
+print(f"Otamning ismi: {Otam['Ism']} {Otam['Tugilgan_yili']} {Otam['Manzil']} da tugilgan")
+
+Onam={
+    'Ism':'Ogiloy',
+    'Tugilgan_yili':'1976-yil',
+    'Manzil':'Chortoq',   
+    }
+print(f"Onam ismi: {Onam['Ism']} {Onam['Tugilgan_yili']} {Onam['Manzil']} da tugilgan")
+
+Men={
+    'Ism':'Mamur',
+    'Tugilgan_yili':'1997-yil',
+    'Manzil':'Chortoq',   
+    }
+print(f"Mening ismim: {Men['Ism']} {Men['Tugilgan_yili']} {Men['Manzil']} da tugilganman")
---
+++ Python Lug'atlar/2-topshiriq.py
@@ -0,0 +1,15 @@
+    #Oila a'zolaringizning sevimli taomlari lug'atini tuzing.
+#Lug'atda kamida 5 ta ism-taom jufltigi bo'lsin.
+#Kamida uch kishining sevimli taomini konsolga chiqaring
+
+ListItems={'Bobir':'Osh',
+           'Qodir':'Shorva',
+           'Jobir':'Shashlik',
+           'Nodir':'Manti',
+           'Sobir':'Lagmon'}
+taom1=ListItems['Jobir']
+taom2=ListItems['Nodir']
+taom3=ListItems['Bobir']
+print(f"Jobirning sevimli taomi: {taom1}\
+      Nodirning sevimli taomi: {taom2}\
+          Bobirning sevimli taomi : {taom3}")
