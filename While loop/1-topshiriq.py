6720483152a3cb9c699b6ad7f7b8abd22a099c76
mamur.zakirov <108054644+mamur1997@users.noreply.github.com>
2025-04-22 21:00:09 +0500

    Update 1-topshiriq.py

--- While loop/1-topshiriq.py
+++ While loop/1-topshiriq.py
@@ -1,1 +1,25 @@
-savol="Istalgan son kiriting? Dasturni toxtatish uchun exit deb yozing \n: "  
+
+
+print('Chiptalar narxi: \n 0-7: 2000 so`m,\n 7-18 3000 so`m,\n'
+      ' 18-65: 10000 so`m \n 65+ : Tekin'
+      )
+savol= "Yoshingizni kiriting: "
+qiymat=True
+while qiymat:
+    qiymat=input(savol)
+    if qiymat =='exit' or qiymat== 'quit':
+        qiymat=False
+    natija=int(qiymat)
+    if natija <= 7:
+        print("Sizga bilet narxi- 10000 so`m")
+        print("Dasturni to`xtatish uchun exit ni kiriting\n")
+    elif natija <=18:
+        print("Sizga bilet narxi- 20000 so`m")
+        print("Dasturni to`xtatish uchun exit ni kiriting\n")
+    elif natija <=65:
+        print("Sizga bilet narxi- 30000 so`m")
+        print("Dasturni to`xtatish uchun exit ni kiriting\n")
+    elif natija >65:
+        print("Sizga tekin")
+        print("Dasturni to`xtatish uchun exit ni kiriting\n")
+
